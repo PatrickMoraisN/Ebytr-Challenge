cd9726d5b3ffe5a6e534d223fba0dc4a03bec5e2
Patrick Morais <ppternunes@gmail.com>
2021-11-03 20:30:36 -0300

    add try/catch

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -2,43 +2,63 @@ import { Request, Response } from 'express';
 import TaskModel from '../models/Task';
 
 export const getAllTasks = async (_req: Request, res: Response) => {
-  const allTasks = await TaskModel.find({});
-  return res.status(201).json({ allTasks });
+  try {
+    const allTasks = await TaskModel.find({});
+    return res.status(201).json({ allTasks });
+  } catch (error) {
+    return res.status(404).json({ message: (error as Error).message });
+  }
 };
 
 export const getTaskById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const task = await TaskModel.findOne({ _id: id });
-  return res.status(201).json({ task });
+  try {
+    const { id } = req.params;
+    const task = await TaskModel.findOne({ _id: id });
+    return res.status(201).json({ task });
+  } catch (error) {
+    return res.status(404).json({ message: (error as Error).message });
+  }
 };
 
 export const editTask = async (req: Request, res: Response) => {
-  const { id, title, description, status, date } = req.body;
-  const newTask = {
-    title,
-    description,
-    status,
-    date,
-  };
-  await TaskModel.updateOne({ _id: id }, newTask);
+  try {
+    const { id, title, description, status, date } = req.body;
+    const newTask = {
+      title,
+      description,
+      status,
+      date,
+    };
+    await TaskModel.updateOne({ _id: id }, newTask);
 
-  return res.status(201).json(newTask);
+    return res.status(201).json(newTask);
+  } catch (error) {
+    return res.status(404).json({ message: (error as Error).message });
+  }
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
-  const { id } = req.body;
-  const deletedTask = await TaskModel.deleteOne({ _id: id });
-  return res.status(201).json(deletedTask);
+  try {
+    const { id } = req.body;
+    const deletedTask = await TaskModel.deleteOne({ _id: id });
+    return res.status(201).json(deletedTask);
+  } catch (error) {
+    return res.status(404).json({ message: (error as Error).message });
+  }
 };
 
 export const createTask = async (req: Request, res: Response) => {
-  const { title, description, status, date } = req.body;
-  const newTask = {
-    title,
-    description,
-    status,
-    date,
-  };
-  await TaskModel.create(newTask);
-  return res.status(201).json(newTask);
+  try {
+    const { title, description, status, date } = req.body;
+    const newTask = {
+      title,
+      description,
+      status,
+      date,
+    };
+    await TaskModel.create(newTask);
+    return res.status(201).json(newTask);
+  } catch (error) {
+    return res.status(404).json({ message: (error as Error).message });
+  }
 };
