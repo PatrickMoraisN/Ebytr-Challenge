12c4913f2be7fb3ca3fdd81a8e17dd3672366e59
Patrick Morais <ppternunes@gmail.com>
2021-11-03 23:07:50 -0300

    add react boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="testezin" disabled={false}>
+        Esse eh um botao simpatico
+      </button>
+    </div>
+  );
+}
+
+export default App;
