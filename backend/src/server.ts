6c99e2c06a805c257c8351b6f0e8a4eb72b8f013
Patrick Morais <ppternunes@gmail.com>
2021-11-04 10:01:31 -0300

    install cors / access localhost:3000

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,19 @@
 /* eslint-disable import/no-unresolved */
 import express from 'express';
 import bp from 'body-parser';
+import cors from 'cors';
 import { mongoConnect } from './database/mongodb';
 import mainRoutes from './routes';
 
 mongoConnect();
 
 const server = express();
+server.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 server.use(bp.json());
 server.use(bp.urlencoded({ extended: true }));
 
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Menu from '../../components/Menu';
+import { api } from '../../services/api';
 import * as S from './style';
 
 function Home() {
+  const getTasks = async () => {
+    const data = await api.get('');
+    console.log(data);
+    return data;
+  };
+  React.useEffect(() => {
+    getTasks();
+  }, []);
   return (
     <div>
       <Menu />
