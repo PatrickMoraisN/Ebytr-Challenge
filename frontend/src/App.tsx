dbc417a121a0179204b3fbabd7f4015f1098ce81
Patrick Morais <ppternunes@gmail.com>
2021-11-03 23:15:17 -0300

    create global style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { CreateGlobalStyle } from './styles/global'
+
 function App() {
   return (
     <div className="App">
       <button type="button" className="testezin" disabled={false}>
         Esse eh um botao simpatico
       </button>
+      <CreateGlobalStyle />
     </div>
   );
 }
---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const CreateGlobalStyle = createGlobalStyle`
+:root {
+  --background: #f0f2f5;
+  --red: #e52e4d;
+  --green: #33cc95;
+  --text-title: #141414;
+  --text-body: #141414;
+  --white: #fff;
+  --purple: #6633cc;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background: var(--background);
+  -webkit-font-smoothing: antialiased; // fonte mais "limpa"
+}
+body, input, textarea, button {
+  font-family: Poppins, sans-serif;
+  font-weight: 400;
+}
+h1, h2, h3, h4, h5, h6, strong {
+  font-family: Poppins, sans-serif;
+  font-weight: 600;
+}
+`;
