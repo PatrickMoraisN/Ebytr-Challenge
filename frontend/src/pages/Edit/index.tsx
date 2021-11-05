9f8a25f30f29bc41191b53741754532d99a8f2af
Patrick Morais <ppternunes@gmail.com>
2021-11-04 22:52:11 -0300

    add edit page

--- frontend/src/pages/Edit/index.tsx
+++ frontend/src/pages/Edit/index.tsx
@@ -1,34 +1,78 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import Input from '../../components/Input';
 import Menu from '../../components/Menu';
+import RadioBox from '../../components/RadioBox';
 import { RootState } from '../../redux/reducers';
-
-type TaskProps = {
-  _id: string;
-  title: string;
-  description: string;
-  status: string;
-  date: string;
-};
+import { api } from '../../services/api';
+import * as S from './style';
 
 function Edit() {
-  // const [teste, setTeste] = React.useState('');
-  const [id, setId] = React.useState(
-    useHistory().location.pathname.split('/')[2]
-  );
+  const [id] = React.useState(useHistory().location.pathname.split('/')[2]);
+  const [title, setTitle] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [status, setStatus] = React.useState('');
   const { tasks } = useSelector((state: RootState) => state.TaskReducer);
-  // React.useEffect(() => {
-  //   setTeste(result);
-  // }, []);
   const filteredTask = tasks.find(({ _id }: any) => _id === id);
-  // console.log(result.tasks);
+  const history = useHistory();
+  React.useEffect(() => {
+    setTitle(filteredTask.title);
+    setDescription(filteredTask.description);
+    setStatus(filteredTask.status);
+  }, []);
+
+  const formatDatePT_BR = (date: string) => {
+    const dateArray = date.split('/');
+    const formatedDate = `${dateArray[1]}/${dateArray[0]}/${dateArray[2]}`;
+    return formatedDate;
+  };
+
+  const handleEditTask = async () => {
+    const unformatedDate = moment().format('l');
+    const date = formatDatePT_BR(unformatedDate);
+    await api.put('/edit', {
+      id,
+      title,
+      description,
+      status,
+      date,
+    });
+    history.push('/home');
+    window.location.reload();
+  };
 
   return (
-    <div>
+    <>
       <Menu />
-      Edit
-    </div>
+      <S.Section>
+        <S.InputContainer>
+          <Input
+            placeholder="Add a Title"
+            inputValue={title}
+            setValue={setTitle}
+          />
+          <Input
+            placeholder="Add a Desc"
+            inputValue={description}
+            setValue={setDescription}
+          />
+        </S.InputContainer>
+        <S.RadioBoxContainer>
+          <RadioBox type={status} setType={setStatus} />
+        </S.RadioBoxContainer>
+        <S.ButtonsContainer>
+          <Link to="/home">
+            <button type="button">Cancel</button>
+          </Link>
+          <button type="button" onClick={handleEditTask}>
+            Edit Task
+          </button>
+        </S.ButtonsContainer>
+      </S.Section>
+    </>
   );
 }
 
---
+++ frontend/src/pages/Edit/style.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  margin-left: 20rem;
+  position: relative;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const RadioBoxContainer = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 30rem;
+  width: 30rem;
+  justify-content: space-between;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  gap: 10px;
+  top: 13rem;
+
+  input {
+    width: 30rem;
+    height: 3.5rem;
+    border: none;
+    border-radius: 6px;
+    font-size: 1.7rem;
+  }
+  input:focus {
+    background: #e3e3e3;
+    outline: none;
+  }
+  input::placeholder {
+    text-align: center;
+    color: var(--purple);
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 13rem;
+
+  button:first-child {
+    background: #d6d6d6;
+    padding: 0.8rem 1.8rem;
+    border-radius: 5px;
+    border: none;
+    transition: 0.3s;
+    color: black;
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  button:nth-child(2) {
+    background: #9bff94;
+    padding: 0.8rem 1.8rem;
+    border-radius: 5px;
+    border: none;
+    transition: 0.3s;
+    color: black;
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  button:hover {
+    filter: brightness(0.8);
+  }
+`;
