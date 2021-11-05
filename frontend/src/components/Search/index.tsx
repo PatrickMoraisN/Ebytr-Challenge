440d382864dc6ecab9d5823c49adc3c8ca75080d
Patrick Morais <ppternunes@gmail.com>
2021-11-05 00:09:14 -0300

    add search only by title

--- frontend/src/components/Search/index.tsx
+++ frontend/src/components/Search/index.tsx
@@ -1,20 +1,62 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import RadioBox from '../RadioBox';
+import { RootState } from '../../redux/reducers';
 import * as S from './style';
 
 type SearchProps = {
-  setTasks: (param: string) => void;
+  setTasks: (param: TaskProps[] | undefined) => void;
+};
+
+type EventChange = {
+  target: HTMLInputElement;
+};
+type TaskProps = {
+  _id: string;
+  title: string;
+  description: string;
+  status: string;
+  date: string;
 };
 
 function Search({ setTasks }: SearchProps) {
-  const [type, setType] = React.useState('');
+  const [type, setType] = React.useState('all');
+  const [searchInput, setSearchInput] = React.useState('');
+
+  const searchOnlyByTitle = (tasks: TaskProps[]) => {
+    const filteredTasks = [...tasks].filter(({ title }) =>
+      title.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())
+    );
+    setTasks(filteredTasks);
+  };
+  const { tasks } = useSelector((state: RootState) => state.TaskReducer);
+  const handleSearch = () => {
+    if (type === 'all') {
+      searchOnlyByTitle(tasks);
+    }
+  };
+
   return (
     <S.SearchContainer>
-      <input type="text" placeholder="Search a name" />
+      <input
+        type="text"
+        placeholder="Search a name"
+        value={searchInput}
+        onChange={({ target }: EventChange) => setSearchInput(target.value)}
+      />
       <S.RadioBoxContainer>
+        <S.RadioBox
+          type="button"
+          isActive={type === 'all'}
+          onClick={() => setType('all')}
+        >
+          <span>All</span>
+        </S.RadioBox>
         <RadioBox type={type} setType={setType} />
       </S.RadioBoxContainer>
-      <S.SearchButton type="button">Search</S.SearchButton>
+      <S.SearchButton type="button" onClick={handleSearch}>
+        Search
+      </S.SearchButton>
     </S.SearchContainer>
   );
 }
--- frontend/src/components/Search/style.ts
+++ frontend/src/components/Search/style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+type RadioBoxProps = {
+  isActive: boolean;
+};
+
 export const SearchContainer = styled.div`
   position: absolute;
   display: flex;
@@ -26,7 +30,7 @@ export const RadioBoxContainer = styled.div`
   position: absolute;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-evenly;
   gap: 15px;
   top: -6rem;
   right: 20rem;
@@ -46,5 +50,23 @@ export const SearchButton = styled.button`
 
   &:hover {
     filter: brightness(0.8);
+  }
+`;
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 3rem;
+  padding: 0 2rem;
+  border: 3px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) => (props.isActive ? '#d7d7d7' : 'transparent')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    filter: brightness(0.9);
+    background: #f7f7f7;
   }
 `;
