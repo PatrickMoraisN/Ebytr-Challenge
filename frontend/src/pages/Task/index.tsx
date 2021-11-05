aeccdd157bd6550b6fff90466fdf7467f3e2afb3
Patrick Morais <ppternunes@gmail.com>
2021-11-04 22:24:48 -0300

    create Task page

--- frontend/src/pages/Task/index.tsx
+++ frontend/src/pages/Task/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { RootState } from '../../redux/reducers';
 import Menu from '../../components/Menu';
 import * as S from './style';
 
 function index() {
+  const [id] = React.useState(useHistory().location.pathname.split('/')[2]);
+
+  const { tasks } = useSelector((state: RootState) => state.TaskReducer);
+  const filteredTask = tasks.find(({ _id }: any) => _id === id);
+
+  const handleClassStatus = (param: string) => {
+    switch (param) {
+      case 'done':
+        return 'done';
+      case 'progress':
+        return 'progress';
+      case 'pending':
+        return 'pending';
+      default:
+        return '';
+    }
+  };
+
   return (
     <>
       <Menu />
-      <S.Section>taskzinha</S.Section>
+      <S.Section>
+        <h2>{filteredTask.title}</h2>
+        <p>{filteredTask.description}</p>
+        <p className={handleClassStatus(filteredTask.status)}>
+          {filteredTask.status}
+        </p>
+        <p>{filteredTask.date}</p>
+        <Link to="/home">
+          <S.ReturnButton type="button">Return</S.ReturnButton>
+        </Link>
+      </S.Section>
     </>
   );
 }
--- frontend/src/pages/Task/style.ts
+++ frontend/src/pages/Task/style.ts
@@ -1,6 +1,53 @@
 import styled from 'styled-components';
 
 export const Section = styled.section`
-  margin-left: 14rem;
-  margin-top: 5rem;
+  position: absolute;
+  width: 50rem;
+  margin-left: 40rem;
+  margin-top: 10rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+
+  h2 {
+    font-size: 4rem;
+  }
+
+  p {
+    font-size: 2rem;
+  }
+  .done {
+    background: #96ff9f;
+    padding: 0.5rem 1rem;
+    border-radius: 6px;
+  }
+
+  .pending {
+    background: #e9fa87;
+    padding: 0.5rem 1rem;
+    border-radius: 6px;
+  }
+
+  .progress {
+    background: #8f9eff;
+    padding: 0.5rem 1rem;
+    border-radius: 6px;
+  }
+`;
+
+export const ReturnButton = styled.button`
+  margin-top: 10rem;
+  text-decoration: none;
+  border: none;
+  font-size: 1.2rem;
+  padding: 0.5rem 1.5rem;
+  border-radius: 4px;
+  background: #d6d6d6;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
 `;
